61d888ddd892da3db1cf7b16088c23f9e38c93ac
Tors Dalid <cletedalid@gmail.com>
2012-07-31 13:48:19 +0800

    fill in delete path for upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,14 +1,15 @@
 class Upload < ActiveRecord::Base
   attr_accessible :upload
-
   has_attached_file :upload
+
+  include Rails.application.routes.url_helpers
 
   def to_jq_upload
     {
       "name" => read_attribute(:upload_file_name),
       "size" => read_attribute(:upload_file_size),
       "url" => upload.url(:original),
-      "delete_url" => '',
+      "delete_url" => upload_path(self),
       "delete_type" => "DELETE" 
     }
   end
