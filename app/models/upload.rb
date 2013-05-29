844eb696708af061d4332bf98363bb4f89683771
SeanMarcia <smarcia@gmail.com>
2013-05-29 11:46:12 -0300

    Update upload.rb
    Updated the upload model to include a thumbnail style as well as have the json return that thumbnail. This way the index will display the thumbnails of uploaded images.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ActiveRecord::Base
   attr_accessible :upload
-  has_attached_file :upload
+  has_attached_file :upload, styles: {thumbnail: "60x60#"}
 
   include Rails.application.routes.url_helpers
 
@@ -9,6 +9,7 @@ class Upload < ActiveRecord::Base
       "name" => read_attribute(:upload_file_name),
       "size" => read_attribute(:upload_file_size),
       "url" => upload.url(:original),
+      "thumbnail_url" => upload.url(:thumbnail),
       "delete_url" => upload_path(self),
       "delete_type" => "DELETE" 
     }
