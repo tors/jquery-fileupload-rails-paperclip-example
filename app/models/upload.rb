3f2e25436a7f7ac2f1f73cf29fda62e75e6ee0ab
Mike Bethany <mike@mikebethany.com>
2014-11-10 13:48:34 -0500

    Moved upload and download templates out of index

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,23 +1,23 @@
-class Upload < ActiveRecord::Base
-  attr_accessible :upload
-  has_attached_file :upload,
-    styles: { 
-      original: "600x600>", 
-      thumbnail: "40x40#" 
-    }
+    class Upload < ActiveRecord::Base
+      attr_accessible :upload
+      has_attached_file :upload,
+        styles: { 
+          original: "600x600>", 
+          thumbnail: "40x40#" 
+        }
   
 
-  include Rails.application.routes.url_helpers
+      include Rails.application.routes.url_helpers
 
-  def to_jq_upload
-    {
-      "name" => read_attribute(:upload_file_name),
-      "size" => read_attribute(:upload_file_size),
-      "url" => upload.url(:original),
-      "delete_url" => upload_path(self),
-      "delete_type" => "DELETE",
-      "thumbnail_url" => upload.url(:thumbnail)
-    }
-  end
+      def to_jq_upload
+        {
+          "name" => read_attribute(:upload_file_name),
+          "size" => read_attribute(:upload_file_size),
+          "url" => upload.url(:original),
+          "delete_url" => upload_path(self),
+          "delete_type" => "DELETE",
+          "thumbnail_url" => upload.url(:thumbnail)
+        }
+      end
 
-end
+    end
